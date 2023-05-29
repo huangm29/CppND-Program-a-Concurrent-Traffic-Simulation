bc73adef92f2ee840711f801439e5e319f27af3b
Meng Huang <huangm1065@gmail.com>
2023-05-29 09:25:41 +0800

    editing

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -6,11 +6,11 @@
 
 template <typename T> T MessageQueue<T>::receive() {
   // FP.5a : The method receive should use std::unique_lock<std::mutex> and
-
-  // to wait for and receive new messages and pull them from the queue using
-  // move semantics.
-  // The received object should then be returned by the receive function.
+  // _condition.wait() to wait for and receive new messages and pull them from
+  // the queue using move semantics. The received object should then be returned
+  // by the receive function.
   std::unique_lock<std::mutex> lock(_mutex);
+  // Wait until the condition is satisfied, i.e., the queue is not empty
   _condition.wait(lock, [this]() { return !_queue.empty(); });
   T item = std::move(_queue.front());
   _queue.pop_front();
@@ -50,8 +50,7 @@ void TrafficLight::simulate() {
   // FP.2b : Finally, the private method „cycleThroughPhases“ should be
   // started in a thread when the public method „simulate“ is called. To do
   // this, use the thread queue in the base class.
-  std::thread thread(&TrafficLight::cycleThroughPhases, this);
-  threads.emplace_back(std::move(thread));
+  threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -62,12 +61,18 @@ void TrafficLight::cycleThroughPhases() {
   // queue using move semantics. The cycle duration should be a random value
   // between 4 and 6 seconds. Also, the while-loop should use
   // std::this_thread::sleep_for to wait 1ms between two cycles.
+  // Create a random device for generating random numbers
   std::random_device rd;
+
+  // Create a random number generator and seed it with the random device
   std::mt19937 gen(rd());
+
+  // Define a uniform distribution with a range between 4000 and 6000
   std::uniform_int_distribution<> distr(4000, 6000);
 
   while (true) {
     auto start = std::chrono::high_resolution_clock::now();
+    // Toggle the traffic light phase between red and green
     _currentPhase == TrafficLightPhase::red ? TrafficLightPhase::green
                                             : TrafficLightPhase::red;
     double cycleDuration = distr(gen);
